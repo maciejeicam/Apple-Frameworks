0bbe4a4db4d8870f8b7cf4e4dc21d718fa7edb30
maciejeicam <maciej_michalik@onet.pl>
2023-03-03 09:06:39 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Maciej Michalik on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
